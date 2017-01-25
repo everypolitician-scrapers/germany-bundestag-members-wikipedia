73e9d4cb67cba359a172246f82bf02eb5ee6670a
Tony Bowden <tony@tmtm.com>
2017-01-25 11:05:40 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'nokogiri'
 require 'open-uri'
@@ -11,7 +12,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -20,11 +21,11 @@ def noko_for(url)
 end
 
 def month(str)
-  ['','januar','februar','märz','april','mai','juni','juli','august','september','oktober','november','dezember'].find_index(str) or raise "Unknown month #{str}".magenta
+  ['', 'januar', 'februar', 'märz', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'dezember'].find_index(str) or raise "Unknown month #{str}".magenta
 end
 
 def date_from(dd, mm, yy)
-  "%02d-%02d-%02d" % [yy, month(mm.downcase), dd]
+  '%02d-%02d-%02d' % [yy, month(mm.downcase), dd]
 end
 
 def scrape_term(id, url)
@@ -35,19 +36,19 @@ def scrape_term(id, url)
     tds = tr.css('td')
     constituency = tds[4].text.tidy rescue ''
     wikiname = tds[0].css('a/@title').first.text
-    data = { 
-      id: wikiname.downcase.tr(' ', '_'),
-      name: tds[0].css('a').first.text.tidy,
-      sort_name: tds[0].css('span[style*="none"]').text,
-      party: tds[2].text.tidy,
+    data = {
+      id:           wikiname.downcase.tr(' ', '_'),
+      name:         tds[0].css('a').first.text.tidy,
+      sort_name:    tds[0].css('span[style*="none"]').text,
+      party:        tds[2].text.tidy,
       constituency: constituency,
-      area: tds[3].text.tidy,
-      term: id,
-      wikiname: wikiname,
-      source: url,
+      area:         tds[3].text.tidy,
+      term:         id,
+      wikiname:     wikiname,
+      source:       url,
     }
 
-    unless (tds[6].nil?) || (notes = tds[6].text.tidy).empty?
+    unless tds[6].nil? || (notes = tds[6].text.tidy).empty?
       data[:notes] = notes
       if sort_date = tds[6].css('span.sortkey')
         sort_date = Date.parse(sort_date.text).to_s rescue nil
@@ -60,22 +61,19 @@ def scrape_term(id, url)
       elsif matched = notes.downcase.match(/verstorben.*?am #{date_re}/)
         data[:death_date] = data[:end_date] = sort_date || date_from(*matched.captures)
       elsif matched = notes.downcase.match(/bis zum #{date_re} .*/)
-        old_name = matched.captures.pop # TODO
+        # TODO: old_name = matched.captures.pop
       elsif matched = notes.downcase.match(/#{date_re} aus der fraktion (.*) ausgeschieden/)
-        # TODO left faktion
+        # TODO: left faktion
       elsif matched = notes.downcase.match(/seit #{date_re} fraktionslos/)
-        # TODO left faktion
+        # TODO: left faktion
       elsif matched = notes.downcase.match(/ab #{date_re} fraktionslos/) || notes.downcase.match(/fraktionslos ab #{date_re}/)
-        # TODO left faktion
+        # TODO: left faktion
       elsif notes.include?('nahm sein Mandat nicht an') || notes.include?('Mandat nicht angenommen')
         # TODO: didn't accept mandate
-      else
-        # warn "#{notes}"
-        # binding.pry
       end
     end
     # puts data
-    ScraperWiki.save_sqlite([:id, :term], data)
+    ScraperWiki.save_sqlite(%i(id term), data)
   end
 end
 
